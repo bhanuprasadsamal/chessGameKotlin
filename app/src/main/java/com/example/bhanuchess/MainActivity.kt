f0eb5a1af87332a6c48d9f3b123f92476f57b70f
(っ◔◡◔)っ ♥ bhanu ♥ <83598884+bhanuprasadsamal@users.noreply.github.com>
2024-04-06 22:00:14 +0530

    chess game bhanu

---
+++ app/src/main/java/com/example/bhanuchess/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.bhanuchess
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = WebViewClient()
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+
+        webView.loadUrl("https://lime-fanni-37.tiiny.site/")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
